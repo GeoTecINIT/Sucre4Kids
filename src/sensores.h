a5c3f339a3cb1deb4553386e19505d26d2711470
Marcos Garcia Garcia <marcosgarciagarcia@Marcoss-Air.technicolor.net>
2022-02-12 18:15:31 +0100

    Metodos sensores creados Si & No versiones

--- src/sensores.h
+++ src/sensores.h
@@ -1,8 +1,36 @@
+boolean noDistancia(int puerto) {}
+boolean siDistancia(int puerto) {}
 
-boolean leerSensorLuz(int puerto, int condicion)
+// boolean leerSensorLuz(int puerto, int condicion)
+// {
+//     Serial.printlnf("Leer Luz");
+
+//     int threshold = 2500;
+//     // Apaño para leer del puerto asignado. AnalogRead(puerto) no funciona.
+//     switch (puerto)
+//     {
+//     case 0:
+//         if (analogRead(A0) >= threshold)
+//             return true;
+//         return false;
+
+//     case 2:
+//         if (analogRead(A2) >= threshold)
+//             return true;
+//         return false;
+
+//     case 4:
+//         if (analogRead(A4) >= threshold)
+//             return true;
+//         return false;
+
+//     default:
+//         return false;
+//     }
+// }
+
+boolean siLuz(int puerto)
 {
-    Serial.printlnf("Leer Luz");
-
     int threshold = 2500;
     switch (puerto)
     {
@@ -26,7 +54,63 @@ boolean leerSensorLuz(int puerto, int condicion)
     }
 }
 
-boolean leerBoton(int puerto)
+boolean noLuz(int puerto)
+{
+    int threshold = 2500;
+    switch (puerto)
+    {
+    case 0:
+        if (analogRead(A0) <= threshold)
+            return true;
+        return false;
+
+    case 2:
+        if (analogRead(A2) <= threshold)
+            return true;
+        return false;
+
+    case 4:
+        if (analogRead(A4) <= threshold)
+            return true;
+        return false;
+
+    default:
+        return false;
+    }
+}
+
+// boolean leerSensorSonido(int puerto)
+// {
+//     // Serial.printlnf("analogico: %d", analogRead(A4));
+//     if (analogRead(puerto) >= 700)
+//         return true;
+//     return false;
+// }
+
+boolean siRuido(int puerto)
+{
+    if (analogRead(puerto) >= 700)
+        return true;
+    return false;
+}
+
+boolean noRuido(int puerto)
+{
+    if (analogRead(puerto) <= 700)
+        return true;
+    return false;
+}
+
+// boolean leerBoton(int puerto)
+// {
+//     pinMode(puerto, INPUT);
+//     Serial.println("Leer boton");
+//     if (digitalRead(puerto) == HIGH)
+//         return true;
+//     return false;
+// }
+
+boolean siBoton(int puerto)
 {
     pinMode(puerto, INPUT);
     Serial.println("Leer boton");
@@ -35,7 +119,31 @@ boolean leerBoton(int puerto)
     return false;
 }
 
-boolean leerAngulo(int puerto)
+boolean noBoton(int puerto)
+{
+    pinMode(puerto, INPUT);
+    Serial.println("Leer boton");
+    if (digitalRead(puerto) == LOW)
+        return true;
+    return false;
+}
+
+// boolean leerAngulo(int puerto)
+// {
+//     float voltage;
+//     int sensor_value = analogRead(puerto);
+//     voltage = (float)sensor_value * 5 / 1023;
+//     float degrees = (voltage * 300) / 5;
+
+//     if (degrees >= 680)
+//     {
+//         // Serial.println(degrees);
+//         return true;
+//     }
+//     return false;
+// }
+
+boolean siRotativo(int puerto)
 {
     float voltage;
     int sensor_value = analogRead(puerto);
@@ -43,45 +151,61 @@ boolean leerAngulo(int puerto)
     float degrees = (voltage * 300) / 5;
 
     if (degrees >= 680)
-    {
-        // Serial.println(degrees);
         return true;
-    }
-
     return false;
 }
 
-boolean leerSensorSonido(int puerto)
+boolean noRotativo(int puerto)
 {
-    // Serial.printlnf("analogico: %d", analogRead(A4));
-    if (analogRead(puerto) >= 700)
+    float voltage;
+    int sensor_value = analogRead(puerto);
+    voltage = (float)sensor_value * 5 / 1023;
+    float degrees = (voltage * 300) / 5;
+
+    if (degrees <= 680)
         return true;
     return false;
 }
+
+boolean tempFrio(int puerto) {}
+boolean tempTemplado(int puerto) {}
+boolean tempCalor(int puerto) {}
 
 boolean leerSensor(int id, int condicion, int puerto)
 {
     switch (id)
     {
+    case 1:
+        return (condicion == 0 ? noDistancia(puerto) : siDistancia(puerto));
+
     case 2:
-        return leerSensorLuz(puerto, condicion);
+        return (condicion == 0 ? noLuz(puerto) : siLuz(puerto));
         break;
 
     case 3:
-        return leerSensorSonido(puerto);
+        return (condicion == 0 ? noRuido(puerto) : siRuido(puerto));
         break;
 
     case 4:
-        return leerBoton(puerto);
+        return (condicion == 0 ? noBoton(puerto) : siBoton(puerto));
         break;
 
     case 5:
-        return leerAngulo(puerto);
+        return (condicion == 0 ? noRotativo(puerto) : siRotativo(puerto));
         break;
 
-    default:
+    case 6:
+        switch (condicion)
+        {
+        case 0:
+            return tempFrio(puerto);
+        case 1:
+            return tempTemplado(puerto);
+        case 2:
+            return tempCalor(puerto);
+        }
 
-        // TODO No entra en el lector MIRAR IMPORTANTE
+    default:
         Serial.println("InvalidSensorError");
         return false;
         break;
